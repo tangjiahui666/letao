e0de36ace57322fcb64405c892886f17009298ec
tangjiahui <tangjiahui@qq.com>
2017-11-21 18:18:58 +0800

    完成了表单校验

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -34,4 +34,24 @@ $(function(){
       }
     }
   });
+  $form.on("success.form.bv",function(e){
+    e.preventDefault();
+    $.ajax({
+      type:"post",
+      url:"/employee/employeeLogin",
+      data:$form.serialize(),
+      success:function(data){
+        if(data.success){
+          location.href="index.html";
+        }
+        if(data.error===1000){
+          $form.data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+        }
+        if(data.error===1001){
+          $form.data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+        }
+      }
+    });
+  });
+  
 });
