c7b99b7ea48834c4035bdd411c19d760a6a1615d
tangjiahui <tangjiahui@qq.com>
2017-11-21 21:54:07 +0800

    首页正式完成（添加登出，侧边高亮）

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -7,3 +7,18 @@ $(".icon_menu").on("click",function(){
     $(".lt_aside").toggleClass("now");
     $(".lt_main").toggleClass("now");
 })
+// 退出功能
+$(".icon_logout").on("click",function(){
+    $("#logoutModal").modal("show");
+    $(".btn_logout").off().on("click",function(){
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            success:function(data){
+                if(data.success){
+                    location.href="login.html";
+                }
+            }
+        })
+    })
+})
