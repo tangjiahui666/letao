350b167cfb3ba08371e70c6c64a43a7c4cab6a03
tangjiahui <tangjiahui@qq.com>
2017-11-21 22:36:41 +0800

    进度条完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,27 @@
+// 进度条
+// 禁用进度环
+NProgress.configure({ showSpinner: false });
+$(document).ajaxStart(function(){
+    NProgress.start();
+});
+$(document).ajaxStop(function(){
+    setTimeout(function(){
+        NProgress.done();
+    },500);
+});
+
+// 非登录页面，判断当前用户是否是登陆了，如果登录了，就继续，如果没登陆，需要跳转到登陆页面
+ if(location.href.indexOf("login.html")==-1){
+    $.ajax({
+        type:"get",
+        url:"/employee/checkRootLogin",
+        success:function(data){
+            if(data.error===400){
+              location.href="login.html";  
+            }
+        }
+    })
+ }
 // 二级分类显示与隐藏
 $(".child").prev().on("click",function(){
     $(this).next().slideToggle();
