20cb99999280ad5f2e98ae572d764903a8812edb
tangjiahui <tangjiahui@qq.com>
2017-11-22 21:52:59 +0800

    二级分类（除了添加）

---
+++ public/back/js/second.js
@@ -0,0 +1,106 @@
+$(function(){
+    var currentPage=1;
+    var pageSize=5;
+    function render(){
+        $.ajax({
+            type:"get",
+            url: "/category/querySecondCategoryPaging",
+            data:{
+                page:currentPage,
+                pageSize:pageSize
+            },
+            success:function(info){
+                $("tbody").html(template("tp1",info));
+                $("#paginator").bootstrapPaginator({
+                    bootstrapMajorVersion: 3,
+                    currentPage: currentPage,
+                    totalPages: Math.ceil(info.total / pageSize),
+                    onPageClicked: function (a, b, c, page) {
+                      currentPage = page;
+                      render();
+                    } 
+                })
+            }
+        })
+    }
+    render();
+
+    $(".btn_add").on("click",function(){
+        $("#addModal").modal("show");
+        $.ajax({
+            type:"get",
+            url:"/category/queryTopCategoryPaging",
+            data:{
+                page:1,
+                pageSize:100
+            },
+            success:function(info){
+                // console.log(info);
+                $(".dropdown-menu").html(template("tp2",info));
+            }
+        });
+    });
+    $(".dropdown-menu").on("click","a",function(){
+        console.log($(this).data("id"));
+        $(".dropdown-text").text($(this).text());
+        $("[name='categoryId']").val($(this).data("id"));
+        $form.data("bootstrapValidator").updateStatus("categoryId","VALID");
+    })
+    $("#fileupload").fileupload({
+        dataType:"json",
+        done:function(e,data){
+            // console.log(data);
+            // console.log(data.result.picAddr);
+            $(".img_box img").attr("src",data.result.picAddr);
+            $("[name='brandLogo']").val(data.result.picAddr);
+            $form.data("bootstrapValidator").updateStatus("brandLogo","VALID");
+        }
+    });
+    var $form=$("form");
+    $form.bootstrapValidator({
+        excluded: [],//不校验的内容
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+          },
+          fields:{
+              categoryId:{
+                  validators:{
+                      notEmpty:{
+                          message:"请选择一级分类"
+                      }
+                  }
+              },
+              brandName:{
+                validators:{
+                    notEmpty:{
+                        message:"请选择二级分类"
+                    }
+                }
+              },
+              brandLogo:{
+                validators:{
+                    notEmpty:{
+                        message:"请上传品牌图片"
+                    }
+                }
+              }
+          }
+    });
+      $form.on("success.form bv",function(e){
+          e.preventDefault();
+          $.ajax({
+            type: "post",
+            url: "/category/addSecondCategory",
+            data: $form.serialize(),
+            success: function (info) {
+              if (info.success) {
+                $("#addModal").modal("hide");
+                currentPage = 1;
+                render();
+                  }
+              }
+          })
+      })
+})
